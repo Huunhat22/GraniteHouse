890bc082882a1193407c8365b8b9973676d58916
Huunhat22 <huunhatth07@gmail.com>
2019-07-08 23:34:13 +0700

    change bootstrap for layout and First commit project

---
+++ Data/GraniteHouseContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Granite_House.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Granite_House.Data
+{
+    public class GraniteHouseContext : IdentityDbContext
+    {
+        public GraniteHouseContext(DbContextOptions<GraniteHouseContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProductType> productTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ProductType>().ToTable("ProductType");
+        }
+    }
+}
---
+++ Data/Migrations/20190708082007_addproducttypetoDB.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Granite_House.Data.Migrations
+{
+    public partial class addproducttypetoDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductType",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductType", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductType");
+        }
+    }
+}
