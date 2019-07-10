34de76af1f29a9faf17ab9984e9cbe05d07850e4
Huunhat22 <huunhatth07@gmail.com>
2019-07-10 16:48:33 +0700

    them ProductTypeController

---
+++ Areas/Admin/Controllers/ProductTypeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Granite_House.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Granite_House.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypeController : Controller
+    {
+        private readonly GraniteHouseContext _context;
+
+        public ProductTypeController(GraniteHouseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
